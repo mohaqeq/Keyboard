06109a9101bcb0ee1bbe53dd13a12ceb7ad48562
Hamidreza Mohaqeq <mohaqeq@outlook.com>
2014-01-15 15:50:28 +0330

    Initial commit

--- index.php
+++ index.php
@@ -6,27 +6,19 @@
     </head>
     <body>
         <?php
-        load();
-        show();
-
-        function load() {
-            include ("KeyboardTraverser.php");
-        }
-
-        function show() {
-            $k = new KeyboardTraverser();
-            $p = $k->getKeySequence("This is a test. We type some characters like #, &, :, / and | for TEST.");
-            echo "<ol>";
-            foreach ($p as $s) {
-                echo "<li>";
-                foreach ($s as $value) {
-                    echo $value;
+        include ("KeyboardTraverser.php");
+        $k = new KeyboardTraverser();
+        $p = $k->getKeySequence("This is a test. We type some characters like #, &, :, / and | for TEST.");
+        echo "<ol>";
+        foreach ($p as $s) {
+            echo "<li>";
+            foreach ($s as $value) {
+                echo $value;
                 echo "<br/>";
-                }
-                echo "</li>";
             }
-            echo "</ol>";
+            echo "</li>";
         }
+        echo "</ol>";
         ?>
     </body>
 </html>
